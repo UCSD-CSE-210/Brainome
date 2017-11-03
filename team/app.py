68813dad191d6c92416f7e94802a5fa4e9dca806
Karan Uppal <k1uppal@eng.ucsd.edu>
2017-11-03 01:07:29 -0700

    Adding the graph Screens and exisiting functionality

--- team/app.py
+++ team/app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask
+from flask_appconfig import AppConfig
+from flask_bootstrap import Bootstrap
 
-@app.route('/')
-def hello_world():
-  return render_template('index.html')
+from .nav import nav
+from .cache import cache
+from .compress import compress, htmlmin
+from .json import MiniJSONEncoder
+
+def create_app(configfile=None):
+    app = Flask(__name__)
+
+    AppConfig(app)
+    Bootstrap(app)
+    # EAM : Set limit on the number of items in cache (RAM)
+    cache.init_app(app, config={'CACHE_TYPE': 'simple', 'CACHE_THRESHOLD': 1000})
+
+    with app.app_context():
+        from .frontend import frontend
+        app.register_blueprint(frontend)
+
+    app.json_encoder = MiniJSONEncoder
+
+    nav.init_app(app)
+
+    compress.init_app(app)
+    htmlmin.init_app(app)
+
+    return app
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  #app.run(debug=True)
+  create_app().start(debug=True)
 
