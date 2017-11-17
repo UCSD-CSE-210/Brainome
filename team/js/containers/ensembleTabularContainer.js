4cb0042d157afbdffe132f951e48a7fef2e63343
DsMaccy <dsmaccy@rocketmail.com>
2017-11-17 11:27:49 -0800

    Update ensembleTabularContainer.js
    Added space

--- team/js/containers/ensembleTabularContainer.js
+++ team/js/containers/ensembleTabularContainer.js
@@ -2,4 +2,4 @@ import EnsembleTabular from '../components/ensembleTabular';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<EnsembleTabular/>,document.getElementById('ensemble_table'));
+ReactDOM.render(<EnsembleTabular/>, document.getElementById('ensemble_table'));
