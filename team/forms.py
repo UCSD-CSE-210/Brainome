c6b9a293d6dc685296f3aee2b895891c8159e855
Karan Uppal <k1uppal@eng.ucsd.edu>
2017-11-19 02:38:03 -0800

    Adding the Change User Email Form

--- team/forms.py
+++ team/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import Form
+from wtforms import ValidationError
 from wtforms.fields import (BooleanField, PasswordField, StringField,
                             SubmitField)
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import Email, EqualTo, InputRequired, Length
+from .user import Role, User
 
 class LoginForm(Form):
     email = EmailField(
@@ -10,3 +12,12 @@ class LoginForm(Form):
     password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
+
+class ChangeUserEmailForm(Form):
+    email = EmailField(
+        'New email', validators=[InputRequired(), Length(1, 64), Email()])
+    submit = SubmitField('Update email')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
