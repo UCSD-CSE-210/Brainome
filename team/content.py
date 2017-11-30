56f49f497af5308a347192c2d67b9bd3962ef466
Haseeb92 <mailforhaseeb@gmail.com>
2017-11-30 08:23:01 -0800

    get_meta_deta functionality added

--- team/content.py
+++ team/content.py
@@ -20,7 +20,9 @@ from plotly.graph_objs import Layout, Box, Scatter, Scattergl, Scatter3d, Heatma
 
 from . import cache
 # from .cluster_color_scale import CLUSTER_COLORS
-
+import sqlite3
+import pandas as pd
+from sqlite3 import Error
 
 class FailToGraphException(Exception):
     """Fail to generate data or graph due to an internal error.s"""
@@ -1378,3 +1380,41 @@ def randomize_cluster_colors():
         randomize_cluster_colors()
 
 
+
+def get_metadata(ensemble = "", postfix = "metadata_example.csv"):
+
+    # datasets = ensemble.split("_")
+    # if len(datasets) > 1:
+    #     datasets = str(tuple(datasets))
+    # else:
+    #     datasets = "('"+ensemble+"')"
+    #
+    # query = "SELECT * FROM dataset_config where Dataset in " + datasets + ";"
+    # try:
+    #     conn = sqlite3.connect(db_file)
+    # except Error as e:
+    #     print(e)
+    #
+    # cur = conn.cursor()
+    # cur.execute(query)
+    #
+    # rows = cur.fetchall()
+    df = pd.DataFrame()
+
+    meta_path = "{}/{}/{}".format(current_app.config['DATA_DIR'], ensemble, postfix)
+
+    # for r in rows:
+    #     if privilege or (r[1] == 1):
+    #         meta_path = r[5] + postfix
+    #         print("Extracting data for: " + meta_path)
+    try:
+        temp = pd.read_csv(meta_path)
+        df = df.append(temp)
+    except:
+        print("File '" + meta_path + "' not found.")
+
+    if len(df) > 0:
+        df = df.reset_index()
+        return df.to_json()
+    else:
+        return False
