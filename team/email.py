ff47c9bfec0685d09a36d6528a68a296319945d3
Karan Uppal <k1uppal@eng.ucsd.edu>
2017-12-01 11:25:27 -0800

    Formatting changes

--- team/email.py
+++ team/email.py
@@ -6,10 +6,7 @@ from . import mail
 
 
 def send_email(recipient, subject, template, **kwargs):
-        msg = Message(
-            'Brainome-Admin' + ' ' + subject,
-            sender='brainome-admin@ucsd.edu',
-            recipients=[recipient])
-        msg.body = render_template(template + '.txt', **kwargs)
-        msg.html = render_template(template + '.html', **kwargs)
-        mail.send(msg)
+    msg = Message('Brainome-Admin' + ' ' + subject, sender='brainome-admin@ucsd.edu',recipients=[recipient])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
