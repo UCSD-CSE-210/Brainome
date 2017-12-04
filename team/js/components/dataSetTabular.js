805a0acdb3321540f12d21df62a8856b392e1e14
meggolbek <mgolbek@eng.ucsd.edu>
2017-12-04 10:27:37 -0800

    finished setting up dataset Tabular screen

--- team/js/components/dataSetTabular.js
+++ team/js/components/dataSetTabular.js
@@ -6,9 +6,9 @@ class MyTable extends React.Component {
 
     constructor(props) {
         super(props);
-        this.rows = [{"dataSet":"DS1","ensemble":"ensemble1","property1":["data1", "data2"]},
-            {"dataSet":"DS2","ensemble":"ensemble2","property1":["data3", "data4"]},
-            {"dataSet":"DS3","ensemble":"ensemble3","property1":["data2", "data5"]}];
+        this.rows = [{"dataSet":"DS1","property1":"xxxx", "property2": "xx"},
+            {"dataSet":"DS2","property1":"xx", "property2": "x"},
+            {"dataSet":"DS3","property1":"xxx", "property2": "xxxx"}];
         this.state = {
             filteredDataList : this.rows,
             sortBy: 'dataSet',
@@ -22,15 +22,15 @@ class MyTable extends React.Component {
             sortDirArrow = this.state.sortDir === 'DESC' ? ' ↓' : ' ↑';
         }
         return <Table
-            height={52+((this.state.filteredDataList.length+1) * 30)}
-            width={475}
+            height={54+((this.state.filteredDataList.length+1) * 30)}
+            width={500}
             rowsCount={this.state.filteredDataList.length}
             rowHeight={30}
             headerHeight={80}
             rowGetter={function(rowIndex) {return this.state.filteredDataList[rowIndex]; }.bind(this)}>
-            <Column dataKey="dataSet" width={200} label={'Data Set'+ (this.state.sortBy === 'dataSet' ? sortDirArrow : '')} headerRenderer={this._renderHeader.bind(this)}/>
-            <Column dataKey="ensemble" width={200} label={'Ensemble'+ (this.state.sortBy === 'ensemble' ? sortDirArrow : '')} headerRenderer={this._renderHeader.bind(this)}/>
+            <Column dataKey="dataSet" width={100} label={'Data Set'+ (this.state.sortBy === 'dataSet' ? sortDirArrow : '')} headerRenderer={this._renderHeader.bind(this)}/>
             <Column  dataKey="property1" width={200} label={'Property 1' + (this.state.sortBy === 'property1' ? sortDirArrow : '')} headerRenderer={this._renderHeader.bind(this)}/>
+            <Column  dataKey="property2" width={200} label={'Property 2' + (this.state.sortBy === 'property2' ? sortDirArrow : '')} headerRenderer={this._renderHeader.bind(this)}/>
         </Table>;
     }
     _onFilterChange(cellDataKey, event) {
